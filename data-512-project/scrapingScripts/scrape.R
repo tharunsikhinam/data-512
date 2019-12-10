09c81a28d58c6dec37130220e6fffb2300ffec0c
tharunsikhinam <tharun.sikhinam@gmail.com>
2019-12-10 15:11:08 -0800

    Update scrape.R

--- data-512-project/scrapingScripts/scrape.R
+++ data-512-project/scrapingScripts/scrape.R
@@ -2,7 +2,7 @@
 # setup -------------------------------------------------------------------
 getwd()
 # load packages / local functions
-source("./scrapingScripts/00-setup.R")
+source("./scrapingScripts/setup.R")
 
 # seasons to scrape
 seasons <- c(2018,2017,2016,2015,2014,2013,2012,2011,2010,2009,2008,2007,2006,2005,2004,2003,2002,2001,2000)
